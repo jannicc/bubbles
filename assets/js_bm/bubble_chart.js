115c892968f0cd820ef356919ff987ce518a71c6
jannicc <80387487+jannicc@users.noreply.github.com>
2021-03-11 09:11:28 +0100

    Add files via upload

--- assets/js_bm/bubble_chart.js
+++ assets/js_bm/bubble_chart.js
@@ -85,38 +85,38 @@ function bubbleChart() {
 // Erster Button: agecat (Alterskategorie)   
  
 var agecatCenters = { // Center locations of the bubbles.
-    1: { x: 150, y: height / 2 },
-    2: { x: 300, y: height / 2 },
-    3: { x: 470, y: height / 2 },
-    4: { x: 600, y: height / 2 },
-    5: { x: 750, y: height / 2 },
-    6: { x: 900, y: height / 2 }
+    1: { x: 100, y: height / 2 },
+    2: { x: 280, y: height / 2 },
+    3: { x: 450, y: height / 2 },
+    4: { x: 580, y: height / 2 },
+    5: { x: 720, y: height / 2 },
+    6: { x: 830, y: height / 2 }
   };
 
   var agecatTitleX = { // X locations of the year titles.
-    'Bis 14 Jahre': 100,
-    '14-15': 250,
-    '16-17': 450,
-    '18-19': 650,
-    '20-29': 800,
-    'Älter als 30 Jahre': 950
+    
+    '14 - 15 Jahre': 200,
+    '16 - 17 Jahre': 450,
+    '18 - 19 Jahre': 650,
+    '20 - 29 Jahre': 780,
+    'Älter als 30 Jahre': 920
   };
     
     
 // Zweiter Button: Geschlecht
     
   var sexCenters = { // Center locations of the bubbles. 
-    'Maennlich': { x: 220, y: height / 2  },
-    'Weiblich': { x: 420, y: height / 2  },
-    'Non-Binaer': { x: 600, y: height / 2  }
+    'Maennlich': { x: 250, y: height / 2  },
+    'Weiblich': { x: 500, y: height / 2  },
+    'Non-Binaer': { x: 750, y: height / 2  }
     
   };
 
   var sexTitleX = {  // X locations of the year titles.
-    'Männer': 150,
-    'Frauen': 450,
-    'Nichtbinär': 650,
-    'Keine Antwort': 850
+    'Männer': 170,
+    'Frauen': 530,
+    'Nichtbinär': 800,
+    
   };
 
 
@@ -168,6 +168,27 @@ var agecatCenters = { // Center locations of the bubbles.
   };  
     
     
+    
+    // facebook
+    
+    
+    
+    var facebookCenters = { // Center locations of the bubbles. 
+    '0': { x: 220, y: height / 2  },
+    '1': { x: 320, y: height / 2  },
+    '2': { x: 420, y: height / 2  },
+    '3': { x: 600, y: height / 2  },
+    '4': { x: 800, y: height / 2  }
+  
+  };
+
+  var facebookTitleX = {  // X locations of the year titles.
+    '"aaa."': 500,
+    'Stimmt ganzbbb': 150,
+    'Stimmt eherccc': 350, 
+    'Stimmt eher nichdddt': 600, 
+    'Stimmt nichtggg': 850
+  };
     
 //* ------------------------------------------------------------------
 //
@@ -235,6 +256,9 @@ var agecatCenters = { // Center locations of the bubbles.
           
         concern: d.sorgenkat,  
         concerntext: d.sorgen,
+          
+        facebook: d.facebcat,
+    
           
         x: Math.random() * 900,
         y: Math.random() * 800
@@ -331,6 +355,7 @@ var agecatCenters = { // Center locations of the bubbles.
     hideSex();
     hideScreentime();
     hideConcern();  
+      hideFacebook();
     
     force.on('tick', function (e) {
       bubbles.each(moveToCenter(e.alpha))
@@ -372,6 +397,7 @@ Die Positionierung basiert auf dem alpha Parameter des force layouts und wird kl
     hideSex();
     hideConcern();
     hideScreentime();
+    hideFacebook();
 
     force.on('tick', function (e) {
       bubbles.each(moveToAgecat(e.alpha))
@@ -419,6 +445,7 @@ function moveToAgecat(alpha) {
     hideAgecat();
     hideConcern();
     hideScreentime();
+      hideFacebook();
 
     force.on('tick', function (e) {
       bubbles.each(moveToSex(e.alpha))
@@ -466,6 +493,7 @@ function moveToAgecat(alpha) {
     hideSex();
     hideAgecat();
     hideConcern();
+      hideFacebook();
 
     force.on('tick', function (e) {
       bubbles.each(moveToScreentime(e.alpha))
@@ -514,6 +542,7 @@ function moveToAgecat(alpha) {
     hideSex();
     hideAgecat();
     hideScreentime();
+      hideFacebook();
 
     force.on('tick', function (e) {
       bubbles.each(moveToConcern(e.alpha))
@@ -550,7 +579,54 @@ function moveToAgecat(alpha) {
       .text(function (d) { return d; });
     }
 
+    //* ------------------------------------------------------------------
+//
+// facebook
+//
+// -----------------------------------------------------------------*/    
     
+  function splitBubblesintoFacebook() {
+    showFacebook();
+      hideAgecat();
+    hideSex();
+    hideConcern();
+    hideScreentime();
+
+    force.on('tick', function (e) {
+      bubbles.each(moveToFacebook(e.alpha))
+        .attr('cx', function (d) { return d.x; })
+        .attr('cy', function (d) { return d.y; });
+    });
+
+    force.start();
+  }
+    
+    
+function moveToFacebook(alpha) {
+    return function (d) {
+      var target = facebookCenters[d.facebook];
+      d.x = d.x + (target.x - d.x) * damper * alpha * 1.1;
+      d.y = d.y + (target.y - d.y) * damper * alpha * 1.1;
+    };
+  }
+
+  function hideFacebook() {
+    svg.selectAll('.facebook').remove();
+  }
+
+  function showFacebook() {
+
+    var facebookData = d3.keys(facebookTitleX);
+    var facebook = svg.selectAll('.facebook')
+      .data(facebookata);
+
+    facebook.enter().append('text')
+      .attr('class', 'facebook')
+      .attr('x', function (d) { return facebookTitleX[d]; })
+      .attr('y', 65)
+      .attr('text-anchor', 'middle')
+      .text(function (d) { return d; });
+    }  
 //* ------------------------------------------------------------------
 //
 // WISSENSCHAFTSWOCHE F
@@ -576,6 +652,8 @@ function moveToAgecat(alpha) {
       splitBubblesintoConcern();
     } else if (displayName === 'screentime') {
       splitBubblesintoScreentime();
+    } else if (displayName === 'facebook') {
+      splitBubblesintoFacebook();
     } else {
       groupBubbles();
     }
@@ -606,7 +684,7 @@ function moveToAgecat(alpha) {
 
   var fillColor = d3.scale.ordinal()
     .domain(['1','2','3', '4','5','6'])
-    .range(['#03045E', '#023E8A', '#0096C7', '#00B4D8','#90E0EF','#CAF0F8']);
+    .range(['#F7CAD0', '#ADE8F4', '#48CAE4', '#0096C7','#023E8A','#03045E']);
 
   /* Tooltip-Funktion*/
   function showDetail(d) {
@@ -624,6 +702,9 @@ function moveToAgecat(alpha) {
                   '</span><br/>' +
                   '<span class="name">"Ich mache mir Sorgen um meine Daten": </span><span class="value">' +
                   d.concerntext +
+                  '</span>';
+                  '<span class="name">"...": </span><span class="value">' +
+                  d.facebook +
                   '</span>';
     tooltip2.showtooltip2(content, d3.event);
   }
